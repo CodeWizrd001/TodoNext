37c9fca54a7c1d3d358c16919b42ea5576f55651
CodeWizrd <codewizard001@gmail.com>
2022-10-07 21:30:05 +0530

    Added Dashboard Page With Dummy Cards

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,46 @@
+import styles from '../styles/Home.module.css'
+
 export default function Dashboard() {
+    const incCard = {
+        title: 'Hello',
+        description: 'Incomplete Todo',
+        completed: false,
+        createdBy: 'Admin'
+    }
+
+    const compCard = {
+        title: 'World',
+        description: 'Completed Todo',
+        completed: true,
+        createdBy: 'Admin'
+    }
+
+    const data = [
+        incCard,
+        compCard
+    ]
+
     return (
         <div>
-            Welcome User !
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome User !
+                </h1>
+
+                <div className={styles.grid}>
+                    {
+                    data.map( (todo) => (
+                        <div className={styles.card}>
+                            <h2>{todo.title}</h2>
+                            <p>{todo.description}</p>
+                            <p1> 
+                                Completed : <input type="checkbox" disabled={true} checked={todo.completed}></input>
+                            </p1>
+                        </div>
+                        )
+                    )}
+                </div>
+            </main>
         </div>
     )
 }
